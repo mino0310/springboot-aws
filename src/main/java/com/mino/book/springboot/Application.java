e4b5842a39538210f342466c9ce826848a4d0df1
mino0310 <bewriter310@gmail.com>
2022-01-23 16:03:58 +0900

    chapter 5 학습 - 스프링 시큐리티와 OAuth2.0으로 로그인 기능 구현

--- src/main/java/com/mino/book/springboot/Application.java
+++ src/main/java/com/mino/book/springboot/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+//@EnableJpaAuditing
 @SpringBootApplication
 public class Application {
-    public static void main(String[] args) {
+    public static void  main(String[] args) {
         // 얘로 인해 내장 WAS를 실행한다.
         SpringApplication.run(Application.class, args);
     }
--- src/main/java/com/mino/book/springboot/web/IndexController.java
+++ src/main/java/com/mino/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.mino.book.springboot.web;
 
+import com.mino.book.springboot.config.auth.LoginUser;
+import com.mino.book.springboot.config.auth.dto.SessionUser;
 import com.mino.book.springboot.domain.posts.service.posts.PostsService;
 import com.mino.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -9,18 +11,24 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.mail.Session;
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
-
     @GetMapping("/posts/save")
     public String postsSave() {
         return "posts-save";
@@ -32,5 +40,4 @@ public class IndexController {
         model.addAttribute("post", dto);
         return "posts-update";
     }
-
 }
